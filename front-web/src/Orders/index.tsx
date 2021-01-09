d88235fd758a373635c1f9305e9829da4776ac0d
andre <plansoft.desenvolvimento@gmail.com>
2021-01-09 16:26:20 -0300

    colocando o quadro do mapa

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,27 @@
 import './style.css'
 import StepsHeader from './StepsHeader'
+import { useEffect, useState } from 'react'
+import {Product} from './types'
+import { fetchProducts } from '../api'
+import ProductsList from './ProductsList'
+import OrderLocation from './OrderLocation'
 
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+    useEffect( () => {
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+
+    }, [] );
+
 return(
+
     <div className="orders-container">
         <StepsHeader />
+        <ProductsList products={products} />
+        <OrderLocation />
     </div>
     )
 }
